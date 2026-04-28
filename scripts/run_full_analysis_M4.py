756280234b7098ef691fe07b3e02e0bb328824da
Justin Chu Jing Hang <justinchu3003@gmail.com>
2026-04-28 16:36:34 +0100

    Add M4 full pipeline script placecholder

---
+++ scripts/run_full_analysis_M4.py
@@ -0,0 +1,12 @@
+"""
+M4 Full Pipeline Script
+
+This script will integrate all modules (M1, M2, M3) and run the full analysis pipeline.
+"""
+# TODO: import M1, M2, and M3 functions
+# TODO: define cloud URL, data folder, and output paths
+# TODO: run download/preparation setup
+# TODO: extract answer sequences
+# TODO: collate answer files
+# TODO: generate summary statistics
+# TODO: visualise possible patterns
